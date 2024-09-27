0085f7d1736ea13052f9e2fc066f0ab64bc67ac6
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-09-27 20:51:47 -0300

    insere dados tabela cars

--- insert_data/12_insert_cars.sql
+++ insert_data/12_insert_cars.sql
@@ -1,2 +1,9 @@
 -- SQLite
--- inserindo dados
+-- inserindo dados tabela cars
+INSERT INTO CARS (NAME, YEAR, COLOR, KM, STATUS, BRAND_ID, MODEL_ID)
+  VALUES ('Chevrolet Onix LT', 2016, 'Preto', 8000.0, 'Liberado', 1, 3),
+        ('Hyundai HB20 1.6', 2022, 'Prata', 3000.0, 'Em manutenção', 3, 3),
+        ('Toyota Yaris', 2021, 'Branca', 10000.0, 'Liberado', 2, 3),
+        ('Fiat Cronos', 2022, 'Preto', 2500.0, 'Liberado', 8, 2),
+        ('Honda HR-V', 2018, 'Prata', 40000.0, 'Em manutenção', 7, 6),
+        ('VW Amarok', 2019, 'Prata', 25000.0, 'Liberado', 4, 6);
