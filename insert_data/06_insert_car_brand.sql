2c29fc968ae9dfc394aa01080c70fcdc400809a2
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-09-27 20:32:09 -0300

    insere dados marcas de carro

---
+++ insert_data/06_insert_car_brand.sql
@@ -0,0 +1,13 @@
+-- SQLite
+
+-- inserindo modelos de carros na tabela car_model
+
+INSERT INTO CAR_BRAND (BRAND_NAME)
+    VALUES ('Chevrolet'),
+           ('Toyota'),
+           ('Hyundai'),
+           ('Volkswagen'),
+           ('Jeep'),
+           ('Renault'),
+           ('Honda'),
+           ('Fiat');
