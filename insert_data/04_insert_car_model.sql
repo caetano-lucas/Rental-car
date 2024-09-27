27ef5989cb891302ef49573c81867ca64f1dd34a
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-09-27 20:30:40 -0300

    cria tabela e insere dados modelos de carro

---
+++ insert_data/04_insert_car_model.sql
@@ -0,0 +1,17 @@
+-- SQLite
+
+-- inserindo modelos de carros na tabela car_model
+
+INSERT INTO CAR_MODEL (MODEL_NAME)
+    VALUES ('Conversível'),
+           ('Sedã'),
+           ('Hatch'),
+           ('Coupé'),
+           ('Perua'),
+           ('SUV'),
+           ('Picape'),
+           ('Minivan'),
+           ('Utilitário'),
+           ('Buggy');
+
+SELECT * FROM CAR_MODEL;
