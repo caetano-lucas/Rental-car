2c1c7f8280a79ebeb5219ef78d3fd221fa4b1ccb
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-09-27 22:26:20 -0300

    altera nomes

--- query/15_select_cars_liberado.sql
+++ query/15_select_cars_liberado.sql
@@ -2,13 +2,13 @@
 -- consulta capaz de exibir somente os automóveis 
 -- que estão com o status “Liberado”
 
-SELECT NAME AS 'NOME',
-       YEAR AS 'ANO',
-       COLOR AS 'COR',
-       KM,
-       STATUS,
-       BRAND_NAME AS 'MARCA',
-       MODEL_NAME AS 'TIPO'
+SELECT CARS.NAME AS 'NOME',
+       CARS.YEAR AS 'ANO',
+       CARS.COLOR AS 'COR',
+       CARS.KM,
+       CARS.STATUS,
+       CAR_BRAND.BRAND_NAME AS 'MARCA',
+       CAR_MODEL.MODEL_NAME AS 'TIPO'
        FROM CARS 
        INNER JOIN CAR_BRAND ON (CARS.BRAND_ID = CAR_BRAND.ID) 
        INNER JOIN CAR_MODEL ON (CARS.MODEL_ID = CAR_MODEL.ID)
