0bc040d8482125a40c0021ca9f93c91985e4df94
Lucas Soares Caetano <caetanolucassoares@gmail.com>
2024-09-27 22:20:21 -0300

    consulta capaz de exibir somente o name, lastname e email dos clientes que moram no estado de SP

--- query/14_select_customers_sp.sql
+++ query/14_select_customers_sp.sql
@@ -3,4 +3,9 @@
 -- consulta capaz de exibir somente o name, lastname e email
 -- dos clientes que moram no estado de SP
 
-SELECT NAME, LASTNAME, EMAIL FROM CUSTOMERS WHERE STATE = 'SP';
+SELECT NAME AS 'NOME',
+       LASTNAME AS 'SOBRENOME', 
+       EMAIL 
+       FROM CUSTOMERS 
+       
+       WHERE STATE = 'SP';
